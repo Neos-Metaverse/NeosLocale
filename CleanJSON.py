5dce734381259aff72b3f5460060fc403f60e806
StoneMoe <lakechan96@gmail.com>
2022-08-21 15:20:44 +0800

    fix CleanJSON.py encoding for non-English OS locale

--- CleanJSON.py
+++ CleanJSON.py
@@ -4,25 +4,25 @@ import json
 
 class NeosLocaleCleaner:
     def __init__(self, en, lang, out):
-        en_file = open(en, 'r')
-        lang_file = open(lang, 'r')
+        en_file = open(en, 'r', encoding='utf-8')
+        lang_file = open(lang, 'r', encoding='utf-8')
         self.out = out
         self.en = en_file.readlines()
         self.lang = json.load(lang_file)
         self.output = []
-        
+
     def run(self):
         self.make_header()
         self.parse()
         self.make_footer()
         self.save()
-    
+
     def make_header(self):
         self.output.append('{')
         self.output.append('    "localeCode": "{}",'.format(self.lang["localeCode"]))
         self.output.append('    "authors": ["{}"],'.format('", "'.join(self.lang["authors"])))
         self.output.append('    "messages": {')
-        
+
     def make_footer(self):
         self.output.append('        "Dummy": "Dummy"')
         self.output.append('    }')
@@ -36,7 +36,7 @@ class NeosLocaleCleaner:
             if "message" in line:
                 break
         return counter
-        
+
     def parse(self):
         start_pos = self.find_start()
         for line in self.en[start_pos:]:
@@ -48,21 +48,21 @@ class NeosLocaleCleaner:
                 self.output.append('        "{}": "{}",'.format(key, translation))
             else:
                 self.output.append('')
-    
+
     def save(self):
-        out = open(self.out, 'w')
+        out = open(self.out, 'w', encoding='utf-8')
         out.write('\n'.join(self.output))
         out.close()
-        
+
 if __name__ == "__main__":
     import argparse
-    
+
     parser = argparse.ArgumentParser(description='This script will reformat a Babel style JSON for locales to match the en.json baseline formating for git changes purposes.')
-    parser.add_argument('--en', metavar='en_path', type=str, 
+    parser.add_argument('--en', metavar='en_path', type=str,
                         help='The path to the en.json Neos locale.')
-    parser.add_argument('--lang', metavar='lang_path', type=str, 
+    parser.add_argument('--lang', metavar='lang_path', type=str,
                         help='The path to the LANG.json Neos locale to clean.')
-    parser.add_argument('--out', metavar='out_path', type=str, 
+    parser.add_argument('--out', metavar='out_path', type=str,
                         help='The path to save the formated file.')
 
     args = parser.parse_args()
